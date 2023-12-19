1ec6a1251716c5e9bfc4abe74fe763011a112051
nsamedaisy <nsamedaisy@gmail.com>
2023-12-19 10:29:05 +0100

    landing page implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import LandingPage from "./landing/page";
 
 export default function Home() {
   return (
     <main className='text-black'>
-      <h1>Hello SecretScribe</h1>
+      {/* <h1>Hello SecretScribe</h1> */}
+      <LandingPage />
     </main>
   )
 }
