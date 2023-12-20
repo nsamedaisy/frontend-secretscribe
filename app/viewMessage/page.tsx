87c83e1de694409e72824d01189f353fde585e14
nsamedaisy <nsamedaisy@gmail.com>
2023-12-20 18:31:23 +0100

    viewMessage page implementation

--- app/viewMessage/page.tsx
+++ app/viewMessage/page.tsx
@@ -8,19 +8,32 @@ const viewSecretMessage = () => {
     <div className="bg-gradient-to-tr from-green to-cream text-white min-h-screen bg-gradie flex items-center justify-center">
       
       <div className="w-[30%] sm:w-[30%] h-[45%] py-10 rounded shadow-2xl px-9 bg-gradient-to-tr from-cream to-green items-center flex flex-col">
-          <h1 className="text-5xl font-extrabold font-sans items-center">My Answers  😅 </h1>
-          <p className="font-semibold w-[20vw] my-4">
+        <h1 className="text-5xl font-extrabold font-sans items-center">My Answers  😅 </h1>
+        <p className="font-semibold w-[20vw] my-4">
           👇 Scroll 👇 down to check out the messages that you have received
-          </p>
+        </p>
 
-          {/* Recieve secretmessage  */}
-          <div>
-            <fieldset />
-          </div>
+        {/* Recieve secret message */}
+        <div className='w-[20vw]'>
+          <fieldset className="border-2 border-cream rounded p-4">
+            <legend className="text-sm font-semibold">Messages:</legend>
+            <div className="flex  flex-col mt-2">
+              <p className="text-base">Message content</p>
+              <p className="text-sm mt-4">_anonymous Time sent</p>
+              <button className='border rounded-xl mt-3 py-1'>Share response</button>
+            </div>
+            {/* Place your received messages and time sent here */}
+          </fieldset>
+        </div>
 
-        <p className="border-b-4 mt-6 w-[20vw]"></p>
+        <button className="flex rounded-xl my-6 justify-center items-center bg-gradient-to-tr from-green to-cream w-[20vw] border-2 border-green py-2">
+        <img src="/time.png" alt="Google logo" className="w-8 h-6 mr-3" />
+            Load More
+          </button>
 
-        <Link href="/profile" className="flex rounded-md my-6 justify-center items-center bg-gradient-to-tr from-green to-cream w-[20vw] border-2 border-green py-2 pl-4">
+        <p className="border-b-2 w-[20vw]"></p>
+
+        <Link href="/profile" className="flex rounded-xl my-6 justify-center items-center bg-gradient-to-tr from-green to-cream w-[20vw] border-2 border-green py-2 pl-4">
             Go back<FaArrowLeft className="ml-3" />
           </Link>
       </div>
@@ -28,4 +41,4 @@ const viewSecretMessage = () => {
   )
 }
 
-export default viewSecretMessage
+export default viewSecretMessage;
--- app/writeMessage/page.tsx
+++ app/writeMessage/page.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+
+import { MdSend } from "react-icons/md";
 
 const writeSecretMessage = () => {
   return (
-    <div>writeSecretMessage</div>
-  )
-}
+    <div className="bg-gradient-to-tr from-green to-cream text-white min-h-screen bg-gradie flex items-center justify-center">
+      <div className="w-[25%] sm:w-[25%] h-[45%] py-10 rounded shadow-2xl px-9 bg-gradient-to-tr from-cream to-green">
+        <h1 className="text-4xl font-extrabold font-sans items-center mb-10">
+          😅 Say Something...
+        </h1>
 
-export default writeSecretMessage
+        {/* Write secret message */}
+        <div className="">
+          <p className="text-sm">What is that you always want to tell me*</p>
+          <fieldset className="border-none">
+            <textarea
+              className="w-full h-40 px-3 py-2 bg-transparent text-white resize-none focus:outline-none"
+              placeholder="Write your secret message..."
+            ></textarea>
+          </fieldset>
+        </div>
+
+        <p className="mt-6 font-mono text-sm">254 characters remaining</p>
+        <hr className="mt-2 border-b-2 w-[20vw]" />
+
+        <Link
+          href="/profile"
+          className="flex rounded-md my-6 justify-center items-center bg-gradient-to-tr from-green to-cream w-[20vw] border-2 border-green py-2 pl-4"
+        >
+          Send Message
+          <MdSend className="ml-3" />
+        </Link>
+
+        <p>
+          Say what do you think about daisyb3ll3 or Leave a feedback for
+          daisyb3ll3 anonymously using the form above.. 🥰 Thank You!! 😍😊
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default writeSecretMessage;
