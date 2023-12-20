17895c78219dd8bfdf03c1598bad8fd62ff70ad4
nsamedaisy <nsamedaisy@gmail.com>
2023-12-20 12:01:49 +0100

    login page implementation

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import Link from "next/link";
 
 // import { FiMenu } from 'react-icons/fi';
-import { FaApple, FaEnvelope, FaFacebook,  } from "react-icons/fa";
+import { FaApple, FaEnvelope, FaFacebookF,  } from "react-icons/fa";
 
 const Login = () => {
   return (
     <div className="bg-cream min-h-screen flex items-center justify-center">
-      <div className="w-[25%] h-[45%] bg-green py-10 shadow-2xl px-9 items-center justify-center ">
+      <div className="w-[25%] h-[45%] bg-green py-10 shadow-2xl px-9">
         <header className="flex justify-between pb-6 items-center">
           <button>Cancel</button>
           <Link
@@ -18,20 +18,20 @@ const Login = () => {
           </Link>
         </header>
 
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center leading-tight">
         <img src="/sslogo.png" alt="SecretScribe Logo" className="hidden md:block w-10 h-10 lg:block" />
-            <h1 className="hidden sm:block text-3xl font-extrabold font-cursive">ecretScribe</h1>
+            <h1 className="hidden sm:block text-3xl font-thin font-marker">ecretScribe</h1>
         </div>
 
-        <h1 className="py-6 text-4xl font-marker items-center justify-center flex font-bold">Welcome back</h1>
+        <h1 className="py-6 text-5xl font-marker items-center justify-center flex font-bold">Welcome back</h1>
 
-        <section className="flex flex-col">
-          <button><img src="/google.png" alt="Google logo" className="w-10 h-10" /> Sign In with Google</button>
-          <button><FaApple className="" /> Sign In with Apple</button>
-          <button><FaEnvelope className="" /> Sign In with Mail</button>
-          <button><FaFacebook className="" /> Sign In with Facebook</button>
+        <section className="flex flex-col space-y-6 bg-orange-600 w-[60%] ml-[20%] mt-8">
+          <button className="flex items-center"><img src="/google.png" alt="Google logo" className="w-8 h-8 mr-3" /> Sign In with Google</button>
+          <button className="flex items-center"><FaApple className="w-6 h-6 mr-3" /> Sign In with Apple</button>
+          <button className="flex items-center"><FaEnvelope className="w-5 h-5 mr-3" /> Sign In with Mail</button>
+          <button className="flex items-center"><FaFacebookF className="w-5 h-5 mr-3" /> Sign In with Facebook</button>
         </section>
-        
+
       </div>
     </div>
   );
