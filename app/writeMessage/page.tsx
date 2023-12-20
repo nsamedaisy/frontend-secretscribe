185c48572d7259ad86d35fefb18adb086c233422
nsamedaisy <nsamedaisy@gmail.com>
2023-12-20 23:01:46 +0100

    implement loader and popup reusable component

--- app/writeMessage/page.tsx
+++ app/writeMessage/page.tsx
@@ -6,14 +6,18 @@ import { MdSend } from "react-icons/md";
 const writeSecretMessage = () => {
   return (
     <div className="bg-gradient-to-tr from-green to-cream text-white min-h-screen bg-gradie flex items-center justify-center">
-      <div className="w-[25%] sm:w-[25%] h-[45%] py-10 rounded shadow-2xl px-9 bg-gradient-to-tr from-cream to-green">
+      <div className="w-[25%] sm:w-[25%] h-[45%] py-10 rounded shadow-2xl px-9 bg-gradient-to-t from-cream to-green">
+
+        {/* success message  */}
+        <p className="text-sm font-extrabold text-lime-100 mb-4 flex justify-center">Message sent successfully 🎉 </p>
+
         <h1 className="text-4xl font-extrabold font-sans items-center mb-10">
           😅 Say Something...
         </h1>
 
         {/* Write secret message */}
         <div className="">
-          <p className="text-sm">What is that you always want to tell me*</p>
+          <p className="text-sm">What is that you always want to tell me <span className="text-red-700">*</span></p>
           <fieldset className="border-none">
             <textarea
               className="w-full h-40 px-3 py-2 bg-transparent text-white resize-none focus:outline-none"
@@ -33,7 +37,7 @@ const writeSecretMessage = () => {
           <MdSend className="ml-3" />
         </Link>
 
-        <p>
+        <p className="text-green">
           Say what do you think about daisyb3ll3 or Leave a feedback for
           daisyb3ll3 anonymously using the form above.. 🥰 Thank You!! 😍😊
         </p>
