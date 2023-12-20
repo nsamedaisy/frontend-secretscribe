31a8d1f051435ad13a75d6f3145ddff3a6856c16
nsamedaisy <nsamedaisy@gmail.com>
2023-12-20 14:14:03 +0100

    register page implementation

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -17,23 +17,39 @@ const Register = () => {
           </Link>
         </header>
 
-        <div className="flex items-center justify-center leading-tight">
+        <div className="flex text-white mb-6 items-center justify-center leading-tight">
           <img
             src="/sslogo.png"
             alt="SecretScribe Logo"
             className="bg-black w-10 h-10"
           />
-          <h1 className="text-3xl font-thin font-marker">ecretScribe</h1>
+          <h1 className="text-3xl my-4 font-thin font-marker">ecretScribe</h1>
         </div>
 
-        <section>
-          <input></input>
+        <section className="flex  flex-col ml-[15%] w-[70%] bg-green">
+          <input
+            placeholder="Email"
+            type="email"
+            className="px-3 border-2 border-cream py-3 bg-transparent text-white focus:outline-none"
+          />
+          <input
+            placeholder="Password"
+            type="password"
+            className="px-3 border-2 border-cream py-3 bg-transparent text-white border-t-0 focus:outline-none"
+          />
         </section>
 
-        <section className="flex flex-col space-y-6 w-[60%] ml-[20%] mt-8 text-white font-extrabold font-lobster mb-5">
-          <button className="flex items-center border-2 border-green py-2 pl-4">
+        <section className="flex flex-col space-y-6 w-[70%] ml-[15%] mt-3 text-white font-extrabold font-lobster mb-5">
+          <button className="flex items-center justify-center border-2 border-green py-2">
             <FaLockOpen className="w-5 h-5 mr-3 text-red-700" /> Sign Up
           </button>
+
+          <p className="flex justify-center items-center py-2">
+            <span className="border-b flex-grow"></span>
+            <span className="px-3 text-gray-500 font-bold">Or</span>
+            <span className="border-b flex-grow"></span>
+          </p>
+
           <button className="flex items-center border-2 border-green py-2 pl-4">
             <img src="/google.png" alt="Google logo" className="w-8 h-8 mr-3" />{" "}
             Sign In with Google
@@ -44,7 +60,7 @@ const Register = () => {
           </button>
         </section>
 
-        <p className="mb-10 items-center text-sm justify-center flex font-serif">
+        <p className="mb-6 items-center text-sm justify-center flex font-serif">
           Educators: Register as an individual
         </p>
       </div>
