05d2b1382647e8f1d4186678eaca0ccf61c69c12
nsamedaisy <nsamedaisy@gmail.com>
2023-12-20 11:24:42 +0100

    login page implementation

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Link from "next/link";
 
+// import { FiMenu } from 'react-icons/fi';
+import { FaApple, FaEnvelope, FaFacebook,  } from "react-icons/fa";
+
 const Login = () => {
   return (
     <div className="text-black">
@@ -14,6 +17,17 @@ const Login = () => {
             Sign Up
           </Link>
         </header>
+        <div className="flex items-center">
+        <img src="/sslogo.png" alt="SecretScribe Logo" className="hidden md:block w-16 h-16 lg:block" />
+            <h1 className="hidden sm:block text-5xl font-extrabold font-marker">ecretScribe</h1>
+        </div>
+        <h1>Welcome back</h1>
+        <section>
+          <button><img src="/google.png" alt="Google logo" className="" /> Sign In with Google</button>
+          <button><FaApple className="" /> Sign In with Apple</button>
+          <button><FaEnvelope className="" /> Sign In with Mail</button>
+          <button><FaFacebook className="" /> Sign In with Facebook</button>
+        </section>
       </div>
     </div>
   );
