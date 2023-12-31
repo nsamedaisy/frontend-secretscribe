b05fab7e0535e5f14111795e0674ede0c8d52f62
nsamedaisy <nsamedaisy@gmail.com>
2023-12-31 09:55:45 +0100

    loader page commit

--- app/components/loader.tsx
+++ app/components/loader.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const Loader = () => {
   return (
-    <div>Loader</div>
-  )
-}
+    <div className="flex items-center justify-center">
+      <img
+        src="/time.png"
+        alt="time logo"
+        className="w-8 h-6 mr-3 animate-spin"
+      />
+    </div>
+  );
+};
 
-export default Loader
+export default Loader;
