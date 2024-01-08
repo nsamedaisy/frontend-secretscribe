770566ba86a2bcb126714d0feeea6ace37b670c4
nsamedaisy <nsamedaisy@gmail.com>
2024-01-08 20:29:52 +0100

    updating profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -12,32 +12,66 @@ import { CurrentUserGuard } from "../_services/ui";
 import { IUser } from "../_services/utils";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { API_URL, FRONT_END_URL } from '../_components/constant';
+import { toast } from 'sonner';
+import axios from "axios";
 
 interface Props {
   currentUser: IUser;
 }
 
 const userProfile = ({ currentUser }: Props) => {
-  const [inputValue, setInputValue] = useState("")
+  const [title, setTitle] = useState("")
+  const [topicLink, setTopicLink] = useState<string>('');
+
 
   const router = useRouter()
 
   const handleChange = (e: any) => {
-    setInputValue(e.target.value)
+    setTitle(e.target.value)
   }
+
+  //generate bucket link
+  const generateLink = (bucket_id: string) => {
+    return `${FRONT_END_URL}/write-message/${bucket_id}`;
+  }
+
+  //copy link function
+  const copyLink = async () => {
+    try {
+      await navigator.clipboard.writeText(topicLink);
+      toast.success("copied!")
+    } catch (error) {
+      console.error('Failed to copy link:', error);
+    }
+  }
+
+  const createBucket = async () => {
+
+    try {
+      const response = await axios.post(API_URL + '/bucket', { title, creator_id: currentUser._id });
+      console.log({ response })
+
+      const { _id } = response.data;
+      const url = generateLink(_id);
+      setTopicLink(url);
+      // Do something with the generated link
+    } catch (error) {
+      console.error(error);
+    }
+    setTitle("")
+  };
+
 
 
   return (
     <div className="bg-gradient-to-tr from-green to-cream text-white min-h-screen bg-gradie flex items-center justify-center">
-      <div className="w-[30%] sm:w-[35%] h-[45%] py-10 rounded shadow-2xl px-9 bg-gradient-to-tr from-cream to-green items-center justify-center flex flex-col">
+      <div className="w-[30%] sm:w-[33%] h-[45%] py-10 rounded shadow-2xl px-9 bg-gradient-to-tr from-cream to-green items-center justify-center flex flex-col">
         <h1 className="text-5xl font-extrabold font-mono mb-4 items-center first-letter:capitalize">
           {currentUser?.name}'s Profile ❤️
         </h1>
-        {/* <Link href="" className="flex items-center py-6 font-bold">
-          share this link and get your response <FaRegCopy className="ml-3" />
-        </Link> */}
 
-        <p className="w-[90%] font-serif font-semibold">
+        <p className="w-[80%] font-serif font-semibold">
           What is it that you want others to talk about without revealing themselves? (e.g Politics 👌, social life ❤️).
           Write a topic of concern and generate your profile link, share ❤️ to get a response from user(s) ❤️. Go to
           "View Messages" to check out the responses. 👌
@@ -48,7 +82,7 @@ const userProfile = ({ currentUser }: Props) => {
           <input
             placeholder="Your topic of interest..."
             type="text"
-            value={inputValue}
+            value={title}
             onChange={handleChange}
             className="px-3 border rounded-md border-cream py-3 bg-transparent text-white focus:outline-none placeholder:text-cream w-[100%]"
           />
@@ -76,10 +110,6 @@ const userProfile = ({ currentUser }: Props) => {
           >
             <FaWhatsapp className="w-6 h-6 mr-3" /> Share on WhatsApp
           </button>
-
-          {/* <a href="/create-bucket" className="flex rounded-md justify-center bg-watGreen py-2 pl-4">
-            <AiTwotoneMail className="w-6 h-6 mr-3 " /> Create a bucket
-          </a> */}
 
           <button className="flex rounded-md justify-center bg-blue-950 py-2 pl-4">
             <FaFacebookSquare className="w-6 h-6 mr-3" /> Share on Facebook
